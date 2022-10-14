344ffb09372de4f1e6c891028e48139cfcbfaf16
ImmortalDante <maklakovfdr@gmail.com>
2022-10-14 11:23:00 +0300

    test api

--- handlers/registration.py
+++ handlers/registration.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 
-from handlers.client.client import start
+from handlers.client.client import start, get_exchange, get_currency
 
 
 def register_client_handlers(dispatcher: Dispatcher):
 	dispatcher.register_message_handler(start, commands=["start", "help", "hello"])
+	dispatcher.register_message_handler(get_exchange, commands=["Kraken"])
+	dispatcher.register_message_handler(get_currency, commands=["BTC"])
